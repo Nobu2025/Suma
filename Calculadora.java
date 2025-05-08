3b326d8f2156ab63fccf1fc132400fd578230cf3
Nobu2025 <20243rd038@utez.edu.mx>
2025-05-08 12:37:55 -0600

    se hizo un programa donde se suma numero1 y numero2

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Calculadora{
+    public static void main (String[] args){
+        Scanner almacenar = new Scanner(System.in);
+            System.out.println("escoge tu opcion");
+            System.out.println("1.- Suma");
+            System.out.println("2.- Resta");
+            System.out.println("3.- Multiplicacion");
+            System.out.println("4.- division");
+            int opc = almacenar.nextInt();
+
+            switch (opc){
+                case 1:
+                    
+                    break;
+            }
+            
+    }
+}
